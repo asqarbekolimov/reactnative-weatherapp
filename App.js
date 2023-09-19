10ff3c42d5a35ac665f0dfee24ab0429026b7a4c
asqarbek olimov <88034288+asqarbekolimov@users.noreply.github.com>
2023-09-19 21:06:13 +0500

    axios get data

--- App.js
+++ App.js
@@ -1,17 +1,25 @@
 import { StatusBar } from "expo-status-bar";
+import axios from "axios";
 import { Alert, StyleSheet, Text, View } from "react-native";
 import Loader from "./components/loader";
 import { useEffect, useState } from "react";
 import Weather from "./components/weather";
 import * as Location from "expo-location";
 
+const API_KEY = "6282e594376d230584878ff2aa9a5cc2";
+
 export default function App() {
   const [isLoading, setIsLoading] = useState(true);
-  const [location, setLocation] = useState({
-    latitude: null,
-    longitude: null,
-  });
+  const [location, setLocation] = useState(null);
   const [errorMsg, setErrorMsg] = useState(null);
+
+  const getWeather = async (latitude, longitude) => {
+    const { data } = await axios.get(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`
+    );
+    setLocation(data);
+    setIsLoading(false);
+  };
 
   const getLoaction = async () => {
     try {
@@ -22,22 +30,17 @@ export default function App() {
       }
 
       const {
-        coords: { altitude, longitude },
+        coords: { latitude, longitude },
       } = await Location.getCurrentPositionAsync({});
-      setLocation({
-        latitude: altitude,
-        longitude: longitude,
-      });
+
+      getWeather(latitude, longitude);
     } catch (error) {
-      Alert.alert("I can`t find your location, so bad");
+      Alert.alert("I can`t find your location, so bad ):");
     }
   };
 
   useEffect(() => {
-    setTimeout(() => {
-      setIsLoading(false);
-      getLoaction();
-    }, 2000);
+    getLoaction();
   }, []);
 
   return isLoading ? <Loader /> : <Weather />;
